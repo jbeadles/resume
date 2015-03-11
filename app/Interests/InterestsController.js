1df3222a1080c250a679815c8937840caee595c0
Beardles <jbeardles@gmail.com>
2015-03-10 19:53:28 -0500

    Set up skeleton for Interests

---
+++ app/Interests/InterestsController.js
@@ -0,0 +1,13 @@
+"use strict";
+
+(function(app) {
+
+    var interestsController = function() {
+
+        var vm = this;
+
+    };
+
+    app.controller("InterestsController", ["dataService", interestsController])
+
+})(angular.module("resumeApp"));
