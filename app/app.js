46559d554292132b682db203a662c247ccc1be6f
Beardles <jbeardles@gmail.com>
2015-03-10 19:42:10 -0500

    Add remaining routes (skills and interests).

--- app/app.js
+++ app/app.js
@@ -12,11 +12,21 @@
                     controller: "AboutMeController",
                     controllerAs: "my"
                 })
+                .when("skills", {
+                    templateUrl: "app/Skills/skills.tmpl.html",
+                    controller: "SkillsController",
+                    controllerAs: "skills"
+                })
                 .when("/employment",
                 {
                     templateUrl: "app/Employment/employment.tmpl.html",
                     controller: "EmploymentController",
                     controllerAs: "employment"
+                })
+                .when("/interests", {
+                    templateUrl: "app/Interests/interests.tmpl.html",
+                    controller: "InterestsController",
+                    controllerAs: "interests"
                 })
                 .otherwise({
                     redirectTo: "/"
